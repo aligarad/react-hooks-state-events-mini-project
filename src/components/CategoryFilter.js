c7e4c5f57280a8f91d4bf2a9ed7210278a4ad296
kenhunja <garadcali190@gmail.com>
2023-01-21 10:59:08 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,31 @@
 import React from "react";
+import React, { useState } from "react";
+
+function CategoryFilter({ categories, onClick }) {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleClick = (category) => {
+    setSelectedCategory(category);
+    onClick(category);
+  };
 
 function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map((category, index) => (
+        <button 
+          key={index} 
+          onClick={() => handleClick(category)}
+          className={category === selectedCategory ? 'selected' : ''}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
+}
 }
 
 export default CategoryFilter;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,37 @@
 import React from "react";
+import React, { useState } from "react";
+
+function NewTaskForm({ categories, onSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ text, category });
+  }};
 
 function NewTaskForm() {
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
         <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={e => setText(e.target.value)} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={e => setCategory(e.target.value)}>
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
     </form>
-  );
-}
+   ) }
+  
+
 
 export default NewTaskForm;
